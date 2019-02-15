88d0a80ffebf981bae23b12e3c65496bcfbde574
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-15 09:21:00 +0300

    importing the module

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template,request,redirect,url_for
-from . import main
-from .forms import ReviewForm , PitchFormP, PitchFormI, PitchFormL, CommentForm
-from ..models import User, Pitch, Review,Comment
-from flask_login import login_required, current_user
+# from flask import render_template,request,redirect,url_for
+# from . import main
+# from .forms import ReviewForm , PitchFormP, PitchFormI, PitchFormL, CommentForm
+# from ..models import User, Pitch, Review,Comment
+# from flask_login import login_required, current_user
 
 # Views
 @main.route('/')
