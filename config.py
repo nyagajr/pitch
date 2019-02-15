32dd9c908062604b72fd10668995f9debf3849bd
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-15 09:41:14 +0300

    database uri

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchoki:123@localhost/pitch'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchoki:123@localhost/pitch'
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
